1e0c25d305891aca109487b92dd2686b42fe0b53
Takuya Horimatsu <new_woory@hotmail.com>
2015-11-14 18:27:53 +0900

    Implemented Sieve of Erathothenes

--- sieve_primes.c
+++ sieve_primes.c
@@ -1,10 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct linkNode {
     int value;
     struct linkNode *next;
 };
+
+void printAllNodes(struct linkNode *start)
+{
+  struct linkNode *current = start;
+  while(1) {
+    if(current->next == NULL) {
+      break;
+    } else {
+      printf("%d\n", current->value);
+      current = current->next;
+    }
+  }
+}
+long countAllNodes(struct linkNode *start)
+{
+  long num = 0;
+  struct linkNode *current = start;
+  while(1) {
+    if(current->next == NULL) {
+      break;
+    } else {
+      current = current->next;
+      num++;
+    }
+  }
+  return num;
+}
+
+void freeAllNodes(struct linkNode *start)
+{
+  struct linkNode *current = start;
+  while(1) {
+    if(current->next == NULL) {
+      break;
+    } else {
+      struct linkNode *old = current;
+      current = current->next;
+      free(old);
+    }
+  }
+}
 
 // 2 3 5 7....
 int main(int argc, char* argv[])
@@ -14,27 +56,55 @@ int main(int argc, char* argv[])
     return 1;
   }
   long max = atoi(argv[1]);
+  int end = ceil(sqrt(max));
   
-  printf("Max = %ld\n", max);
+  printf("Max = %ld End = %d \n", max, end);
   struct linkNode *start;
   struct linkNode *current;
   start = (struct linkNode *)malloc(sizeof(struct linkNode));
   current = start;
 
-  for(int i = 0;i < max;i++) {
+  current->next = (struct linkNode *)malloc(sizeof(struct linkNode));
+  current->next->next = NULL;
+  current->value = 2;
+  current = current->next;
+
+  for(int i = 2;i < max;i += 2) {
     current->next = (struct linkNode *)malloc(sizeof(struct linkNode));
     current->next->next = NULL;
     current->value = i + 1;
     current = current->next;
   }
   current = start;
+
   while(1) {
-    if(current->next == NULL) {
+    if(current->next == NULL || current->next->value == 0) {
       break;
-    } else {
-      printf("%d\n", current->value);
-      current = current->next;
+    } 
+    int num = current->value;
+    // printf("N = %d\n", num);
+
+    struct linkNode *seek = current;
+    while(1) {
+      // printf("seek.value = %d\n", seek->value);
+      if(seek->next == NULL || seek->next->value == 0) {
+        break;
+      }
+      if(seek->next->value % num == 0) {
+          // printf("seek.next.value = %d\n", seek->next->value);
+          struct linkNode *old = seek->next;
+          seek->next = seek->next->next;
+          free(old);
+      }
+      seek = seek->next;
+      if(seek == NULL) {
+        break;
+      }
     }
+    current = current->next;
   }
+  printf("%ld primes\n", countAllNodes(start));
+
+  freeAllNodes(start);
   return 0;
 }
