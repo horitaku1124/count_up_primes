242c9887561300c0445f1b79684160884febf133
Takuya Horimatsu <new_woory@hotmail.com>
2017-01-23 23:20:34 +0900

    optimize code

--- sieve_primes2.c
+++ sieve_primes2.c
@@ -10,6 +10,7 @@ int main(int argc, char* argv[])
     printf("arg error");
     return 1;
   }
+  long count = 0;
   long max = atoi(argv[1]);
   if(max < 2) {
     printf("max error");
@@ -22,19 +23,15 @@ int main(int argc, char* argv[])
   printf("Max = %ld End = %d \n", max - 1, end);
   list[0] = 1;
   list[1] = 1;
- 
+
   for(int sieve = 2;sieve <= end;sieve++) {
     if(list[sieve] == 1) continue;
-    int limit = max / sieve;
-
-    for(int seek = 2;seek <= limit;seek++) {
+    for(int index = sieve * 2;index < max;index += sieve) {
       // printf("limit = %d seek = %d sieve = %d\n", limit, seek, sieve);
-      int index = sieve * seek;
       list[index] = 1;
     }
   }
 
-  long count = 0;
   for(int i = 2;i < max;i++) {
     // printf("%d = %d\n", i, list[i]);
     if(list[i] == 0) {
